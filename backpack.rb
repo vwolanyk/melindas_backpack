327a0f48a5b8c41a788aad6a46581e353c0ac682
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-26 15:03:01 -0400

    Refactor prepare method, add gymday? and pack_food method to backpack class

--- backpack.rb
+++ backpack.rb
@@ -18,18 +18,22 @@ class Backpack
     add_clothing(weather)
 
     # Ensure gym shoes are added to backpack if it's a gym day
-    if day_of_week == 'monday' || day_of_week == 'thursday'
-      # add gym shoes to items
-      @items << 'gym shoes'
-    end
+      @items << 'gym shoes' if gym_day?(day_of_week)
 
     # Bring a packed lunch on all weekdays
+    pack_food(day_of_week)
+  end
+
+  def gym_day?(day_of_week)
+    day_of_week == 'monday' || day_of_week == 'thursday'
+  end
+
+  def pack_food(day_of_week)
     if day_of_week != 'saturday' && day_of_week != 'sunday'
       @items << 'packed lunch'
     elsif false
       @items << 'snacks'
     end
-
   end
 
   def add_clothing(weather)
