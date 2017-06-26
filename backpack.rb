7e221bb08a1e9b343c8870fa75b3b61991c5179e
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-26 14:30:46 -0400

    Refactor Prepare method variable x to weather

--- backpack.rb
+++ backpack.rb
@@ -11,15 +11,15 @@ class Backpack
 
   def prepare
     # set up local variables used by rest of prepare method
-    x = @attributes[:weather]
+    weather = @attributes[:weather]
     day_of_week = @attributes[:day_of_week]
 
     # Ensure appropriate clothing is added to backpack
-    if x == 'rainy'
+    if weather == 'rainy'
       @items << 'pants'
       @items << 'shirt'
       @items << 'umbrella'
-    elsif x == 'cold'
+    elsif weather == 'cold'
       @items << 'pants'
       @items << 'shirt'
       @items << 'jacket'
