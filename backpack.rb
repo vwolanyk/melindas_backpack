98160b0096c7de10fe4ce52c8f80d0f7a9d51843
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-26 14:54:30 -0400

    Refactor - new method add_clothing extracted from prepare method

--- backpack.rb
+++ backpack.rb
@@ -15,22 +15,11 @@ class Backpack
     day_of_week = @attributes[:day_of_week]
 
     # Ensure appropriate clothing is added to backpack
-    if weather == 'rainy'
-      @items << 'pants'
-      @items << 'shirt'
-      @items << 'umbrella'
-    elsif weather == 'cold'
-      @items << 'pants'
-      @items << 'shirt'
-      @items << 'jacket'
-    else
-      @items << 'pants'
-      @items << 'shirt'
-    end
+    add_clothing(weather)
 
     # Ensure gym shoes are added to backpack if it's a gym day
     if day_of_week == 'monday' || day_of_week == 'thursday'
-      #add gym shoes to items
+      # add gym shoes to items
       @items << 'gym shoes'
     end
 
@@ -38,8 +27,20 @@ class Backpack
     if day_of_week != 'saturday' && day_of_week != 'sunday'
       @items << 'packed lunch'
     elsif false
-
       @items << 'snacks'
+    end
+
+  end
+
+  def add_clothing(weather)
+    # Always Add
+    @items << 'pants'
+    @items << 'shirt'
+
+    if weather == 'rainy'
+        @items << 'umbrella'
+    elsif weather == 'cold'
+      @items << 'jacket'
     end
   end
 
