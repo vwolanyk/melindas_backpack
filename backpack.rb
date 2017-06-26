cc36bc7b2dc58d0b7a13c60fad899aefb974dfe1
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-26 14:32:57 -0400

    Refactor, remove inappropriate comments from prepare method

--- backpack.rb
+++ backpack.rb
@@ -29,7 +29,6 @@ class Backpack
     end
 
     # Ensure gym shoes are added to backpack if it's a gym day
-    # Gotta get to the gym on Tuesdays and Thursdays. Wait a sec...
     if day_of_week == 'monday' || day_of_week == 'thursday'
       #add gym shoes to items
       @items << 'gym shoes'
